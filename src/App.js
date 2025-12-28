cc3afc6eeb1ce17c4d36aa5903ed25f7d3ec9ad9
Maddela Archana <archanamaddela@gmail.com>
2025-12-28 18:53:17 +0530

    updated codes for ProtectedRoute and App js files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+import ProtectedRoute from "./ProtectedRoute";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/admin" element={<ProtectedRoute>
+          <AdminDashboard/>
+        </ProtectedRoute>}/>
+        <Route path="*" element={<Navigate to="/login"/>}/>
+      </Routes>
+    </Router>
   );
 }
 
